353cafb51448c2c1390a047a5e24634a38cbc332
Jesse Li <jesse@sandboxx.us>
2018-06-01 13:18:27 -0400

    Add ThreeTenABP

--- app/src/main/java/com/lithiumsheep/stratechery/StratecheryApp.java
+++ app/src/main/java/com/lithiumsheep/stratechery/StratecheryApp.java
@@ -2,6 +2,8 @@ package com.lithiumsheep.stratechery;
 
 import android.app.Application;
 
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
 import timber.log.Timber;
 
 public class StratecheryApp extends Application {
@@ -13,5 +15,7 @@ public class StratecheryApp extends Application {
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
+
+        AndroidThreeTen.init(this);
     }
 }

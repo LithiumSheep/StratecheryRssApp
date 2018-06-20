21d6e5899a85299f2382b3aca484f16c3bcb9230
Jesse Li <jesse@sandboxx.us>
2018-06-20 15:31:00 -0400

    AccountHeader

--- app/src/main/java/com/lithiumsheep/stratechery/ui/MaterialDrawerActivity.java
+++ app/src/main/java/com/lithiumsheep/stratechery/ui/MaterialDrawerActivity.java
@@ -3,10 +3,13 @@ package com.lithiumsheep.stratechery.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.lithiumsheep.stratechery.R;
 import com.mikepenz.community_material_typeface_library.CommunityMaterial;
 import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
@@ -20,11 +23,19 @@ public abstract class MaterialDrawerActivity extends AppCompatActivity implement
     private Drawer drawer;
 
     protected final void attachDrawer(Toolbar toolbar) {
+        AccountHeader header =
+                new AccountHeaderBuilder()
+                        .withActivity(this)
+                        .withHeaderBackground(R.drawable.stratechery_banner_large)
+                        .withHeaderBackgroundScaleType(ImageView.ScaleType.FIT_CENTER)
+                        .build();
+
         drawer = new DrawerBuilder()
                 .withActivity(this)
                 .withToolbar(toolbar)
+                .withAccountHeader(header)
                 .addDrawerItems(
-                        new SectionDrawerItem().withName("Explore").withDivider(false).withTextColorRes(R.color.colorPrimary),
+                        new SectionDrawerItem().withName("Explore").withDivider(false),
                         new PrimaryDrawerItem().withName("Concepts").withIcon(CommunityMaterial.Icon.cmd_lightbulb_on),
                         new PrimaryDrawerItem().withName("Companies").withIcon(GoogleMaterial.Icon.gmd_business),
                         new PrimaryDrawerItem().withName("Topics").withIcon(CommunityMaterial.Icon.cmd_dictionary), // library, books, clipboard

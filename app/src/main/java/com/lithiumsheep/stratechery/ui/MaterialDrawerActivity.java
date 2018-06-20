4d4307a618f1fbbbfc8b44fa71837c605bdab367
Jesse Li <jesse@sandboxx.us>
2018-06-20 15:24:42 -0400

    Add some drawer icons

--- app/src/main/java/com/lithiumsheep/stratechery/ui/MaterialDrawerActivity.java
+++ app/src/main/java/com/lithiumsheep/stratechery/ui/MaterialDrawerActivity.java
@@ -2,14 +2,20 @@ package com.lithiumsheep.stratechery.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
+import com.lithiumsheep.stratechery.R;
+import com.mikepenz.community_material_typeface_library.CommunityMaterial;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.DividerDrawerItem;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
-public abstract class MaterialDrawerActivity extends AppCompatActivity {
+public abstract class MaterialDrawerActivity extends AppCompatActivity implements Drawer.OnDrawerItemClickListener {
 
     private Drawer drawer;
 
@@ -18,12 +24,36 @@ public abstract class MaterialDrawerActivity extends AppCompatActivity {
                 .withActivity(this)
                 .withToolbar(toolbar)
                 .addDrawerItems(
-                        new PrimaryDrawerItem(),
+                        new SectionDrawerItem().withName("Explore").withDivider(false).withTextColorRes(R.color.colorPrimary),
+                        new PrimaryDrawerItem().withName("Concepts").withIcon(CommunityMaterial.Icon.cmd_lightbulb_on),
+                        new PrimaryDrawerItem().withName("Companies").withIcon(GoogleMaterial.Icon.gmd_business),
+                        new PrimaryDrawerItem().withName("Topics").withIcon(CommunityMaterial.Icon.cmd_dictionary), // library, books, clipboard
                         new DividerDrawerItem(),
-                        new SecondaryDrawerItem()
+                        new SecondaryDrawerItem().withName("About").withIcon(GoogleMaterial.Icon.gmd_info),
+                        new SecondaryDrawerItem().withName("Settings").withIcon(GoogleMaterial.Icon.gmd_settings),
+                        new SecondaryDrawerItem().withName("Support Development").withIcon(CommunityMaterial.Icon.cmd_heart),
+                        new SecondaryDrawerItem().withName("Open Source").withIcon(CommunityMaterial.Icon.cmd_github_circle)
                 )
+                .withOnDrawerItemClickListener(this)
                 .withSelectedItem(-1)
                 .build();
+    }
+
+    @Override
+    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 
     protected abstract boolean shouldMoveTaskToBackOnBackPressed();
